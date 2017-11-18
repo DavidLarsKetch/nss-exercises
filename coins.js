d3c4f561b01eefb1d44648d7a3738fe06204c6cf
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-18 14:46:39 -0600

    Req 1: counts coins with rounding errors accounted for

--- coins.js
+++ coins.js
@@ -1,23 +1,30 @@
-/*
-  Input: 0.67
-  Output:
-  {
-    quarters: 2,
-    dimes: 1,
-    nickels: 1,
-    pennies: 2
+function coinCounter (val) {
+  // Initialize a JavaScript object to hold the coins
+  const coinData = {
+    quarter: 25,
+    dime: 10,
+    nickel: 5,
+    penny: 1
   }
 
-*/
+  const coinPurse = {
+    quarter: 0,
+    dime: 0,
+    nickel: 0,
+    penny: 0
+  };
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
-  
-  coinPurse.quarters = 0;
+  let val100 = val * 100; //Javascript is bad at decimals, converts dollar amount to integer
+
+  for (let i in coinData) {
+    while ((val100 - coinData[i]) >= 0) {
+      val100 -= coinData[i];
+      coinPurse[i]++;
+    }
+  }
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+var coins = coinCounter(1.27);
+console.log(coins);
