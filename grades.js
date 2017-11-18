ad71cb87bcb45a7e9fbeec852b5529e366981487
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-17 23:51:50 -0600

    Initializes grades.js

---
+++ grades.js
@@ -0,0 +1,11 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+//for (let i = 0; i > scores.length; i++) {
+//  switch (scores[i]) {
+//  case scores[i] >= 91:
+//    
+//  }
+//}
+
+scores.sort();
+console.log(scores);
