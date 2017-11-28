4781f52e9b5eba3a89abde0b8162753ca567d0c4
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-28 15:43:45 -0600

    Req 5: Replaces and prints sonnet with 'yuletide' instead of 'winter'

--- sonnet.js
+++ sonnet.js
@@ -1,3 +1,5 @@
 let sonnet = document.getElementById("sonnet").innerHTML;
 console.log(sonnet.search("orphans"));
 console.log(sonnet.length);
+sonnet = sonnet.replace("winter", "yuletide");
+console.log(sonnet);
