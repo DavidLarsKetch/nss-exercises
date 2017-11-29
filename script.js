179e20c458d9196c5a559c5d23938c2767bc7a8e
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-29 14:57:59 -0600

    Defines splitSquareAdd function

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const printToDOM = document.getElementById("printHere");
 let end = 0;
 let happyNums = [];
 let num = 1;
+
+function splitSquareAdd(workingNum) {
+  let workingArr = workingNum.toString().split('');
+  workingArr = workingArr.map(x => x**2);
+  let end = 0;
+  for (let i = 0; i < workingArr.length; i++) {
+    end += workingArr[i];
+  }
+  return end;
+}
+
