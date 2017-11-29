2cf33342cb007c58a53648138e96e26beee91ad8
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-29 15:45:57 -0600

    Defines add func

--- calculate.js
+++ calculate.js
@@ -3,3 +3,7 @@ const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 const output = document.getElementById("output");
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
