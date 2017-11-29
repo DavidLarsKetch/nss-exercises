01b5bbb2f9a992ed3f767f61cdc3f90379f11b66
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-29 11:48:48 -0600

    Adds all makeCard obj method

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const CardMaker = {
   makeCard: function() {
+    const container = document.getElementById("container");
+    const cardElm = document.createElement("div");
+
+    cardElm.className = "card";
+    cardElm.id = this.assignID();
+    container.appendChild(cardElm);
+
+    this.makeFrontMessage(cardElm);
+    this.makeBodyMessage(cardElm);
+    this.makeFooter(cardElm);
+    this.addListener();
   },
   makeFrontMessage: function(cardElm) {
   },
