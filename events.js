863e34ab856581ec2527fe11e4e570d559f5f308
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-05 14:47:25 -0600

    Req 5: mousing over <h1> prints innerHTML to output-target; fixes typos

--- events.js
+++ events.js
@@ -1,8 +1,15 @@
 const outputTarget = document.getElementById("output-target");
 
 document.body.addEventListener("click", event => {
-  let clickTarget = event.target
+  let clickTarget = event.target;
   if (clickTarget.localName === "section") {
     outputTarget.innerHTML = `You clicked on the ${clickTarget.innerHTML} section`;
+  }
+});
+
+document.body.addEventListener("mouseover", event => {
+  let hoverTarget = event.target;
+  if (hoverTarget.localName === "h1") {
+    outputTarget.innerHTML = `You moved your mouse over the header.`;
   }
 });
