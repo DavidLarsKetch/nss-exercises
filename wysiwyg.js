a64c88cb1a3cb4befed0febd3b630073e76ea3f4
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-06 08:59:16 -0600

    Req 1: Creates array of famous people objs; initializes .js

---
+++ wysiwyg.js
@@ -0,0 +1,52 @@
+const famousContinentals = [
+  {
+    expertise: "Epistemology",
+    name: "Edmund Husserl",
+    bio: "Mathematician turned philosopher, Husserl began the Phenomenological movement.",
+    image: "https://commons.wikimedia.org/wiki/File:Edmund_Husserl_1910s.jpg#/media/File:Edmund_Husserl_1910s.jpg",
+    lifespan: {
+      birth: 1859,
+      death: 1938
+    }
+  },
+  {
+    expertise: "Ontology",
+    name: "Martin Heidegger",
+    bio: "Husserl's protege, Heidegger took Phenomenology into uncharted territory.",
+    image: "https://commons.wikimedia.org/wiki/File:Edmund_Husserl_1910s.jpg#/media/File:Edmund_Husserl_1910s.jpg",
+    lifespan: {
+      birth: 1889,
+      death: 1976
+    }
+  },
+  {  
+    expertise: "Ethics",
+    name: "Max Scheler",
+    bio: "A contemporary of Husserl's, Scheler envisioned a form of Phenomenology that both complemented and competed with Husserl's",
+    image: "https://commons.wikimedia.org/wiki/File:Scheler_max.jpg#/media/File:Scheler_max.jpg",
+    lifespan: {
+      birth: 1874,
+      death: 1928
+    }
+  },
+  {
+    expertise: "Epistemology",
+    name: "Maurice Merleau-Ponty",
+    bio: "A Frenchman, Merleau-Ponty applied Husserl's insights to a more psychologically informed perspective.",
+    image: "https://commons.wikimedia.org/wiki/File:Maurice_Merleau-Ponty.jpg#/media/File:Maurice_Merleau-Ponty.jpg",
+    lifespan: {
+      birth: 1908,
+      death: 1961
+    }
+  },
+  {
+    expertise: "Existentialism",
+    name: "Jean-Paul Sartre",
+    bio: "Often considered the seminal existentialist thinker, Sartre used Phenomenology to describe and discuss the existential and political situation of humankind.",
+    image: "https://commons.wikimedia.org/wiki/File:Sartre_1967_crop.jpg#/media/File:Sartre_1967_crop.jpg",
+    lifespan: {
+      birth: 1905,
+      death: 1980
+    }
+  },
+];
