b02ee4f93486d734e4f6413e34b3dc30a40be068
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-08 13:47:18 -0600

    Req 6: palidrome() function meets req; prints reversal & alphabits to DOM

--- strings.js
+++ strings.js
@@ -1,15 +1,24 @@
+const output = document.getElementById("btnInput");
+
 function reversal(str) {
   let newStr = str.split('').reverse().join('');
+  return newStr
 }
 
 function alphabits(str) {
-  let newStr = str.toLowerCase().split('').sort().join('')
+  let newStr = str.toLowerCase().split('').sort().join('');
+  return newStr
 }
 
-function palindrome() {
+function palindrome(str) {
+  let check = reversal(str) === str ? true : false;
+  if(check) {
+    output.insertAdjacentHTML("afterend", "<div>Your string is a palindrome</div>");
+  }
 }
 
-var testString = "Hey there!";
-reversal(testString);
-alphabits(testString);
+var testString = "racecar";
+let reverse = reversal(testString);
+let bits = alphabits(testString);
 palindrome(testString);
+output.insertAdjacentHTML("afterend", `<div>${reverse}</div><div>${bits}</div>`);
