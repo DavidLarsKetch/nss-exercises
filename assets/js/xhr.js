0bf3f718a82a2f919dac5f83e2b500a442f35f8c
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-08 09:35:58 -0600

    Adds requires & vars in xhr.js

---
+++ assets/js/xhr.js
@@ -0,0 +1,7 @@
+"use strict";
+const $ = require('jquery');
+const random = require('lodash/random');
+
+const from = "Cohort-23";
+const slurs = ["back", "blackadder", "bus", "cocksplat", "donut", "gfy", "ing", "keep", "king", "off", "outside", "problem", "yoda", "you"];
+
