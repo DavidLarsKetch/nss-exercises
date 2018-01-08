52799c4d480f80bfeab7faf5048f6750364415dd
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-08 09:34:54 -0600

    Defines organizeDates() fn in events.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -6,6 +6,15 @@ const wrongDatesTemplate = require("../templates/partials/wrong.hbs");
 let arr = [];
 const range = {};
 
+function organizeDates() {
+  //Reorganizes in case user enters 'to' date before 'for' date
+  arr.push($("input[name='from']").val(), $("input[name='to']").val());
+  arr.sort();
+  range.from = arr[0];
+  range.to = arr[1];
+  arr = [];
+}
+
 module.exports.addBtnClick = (fn) => {
   $("#enter").click(() => {
     organizeDates();
