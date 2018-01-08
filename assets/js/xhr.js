a9fdf9acbcdd574c98da064d609168215ccb5cd1
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-08 09:36:33 -0600

    Defines & exports FOAAS Promise in xhr.js

--- assets/js/xhr.js
+++ assets/js/xhr.js
@@ -20,3 +20,22 @@ module.exports.nasa = selection => {
   });
 };
 
+module.exports.foaas = selection => {
+  let num = random(0, slurs.length - 1);
+
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      accepts: {
+        json: 'application/json'
+      },
+      dataType: 'json',
+      url: `http://www.foaas.com/${slurs[num]}/${selection}/${from}`
+    })
+    .done(data => {
+      resolve(data);
+    })
+    .fail(err => {
+      reject(err);
+    });
+  });
+};
