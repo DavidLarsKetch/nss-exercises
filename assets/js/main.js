fcaba5bf8a5a8be123d28941ab60afa99b8ede2a
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-08 09:36:55 -0600

    Defines XHR calls in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,25 @@ const xhr = require('./xhr');
 const listener = require('./events');
 const processor = require("./processor");
 
+const callXHR = selection => {
+  $("#output").empty();
+  xhr.nasa(selection)
+  .then(data => {
+    let names = processor.process(data);
+    names.forEach(name => {
+      xhr.foaas(name)
+      .then(msg => {
+        $("#output").append(foTemplate(msg));
+      })
+      .catch(err => {
+        ("#output").append(errTemplate);
+      });
+    });
+  })
+  .catch(err => {
+    $("#output").append(errTemplate);
+  });
+};
+
 $("#container").append(foaasTemplate);
 listener.addBtnClick(callXHR);
