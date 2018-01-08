0f9f972dd2d5828b138e00e1141796bfe7cc83f3
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-08 09:34:29 -0600

    Exports addBtnClick from events.js

--- assets/js/events.js
+++ assets/js/events.js
@@ -6,3 +6,18 @@ const wrongDatesTemplate = require("../templates/partials/wrong.hbs");
 let arr = [];
 const range = {};
 
+module.exports.addBtnClick = (fn) => {
+  $("#enter").click(() => {
+    organizeDates();
+
+    let result = check();
+
+    if(result) {
+      fn(range);
+    } else {
+      $("#output").empty();
+      $("#output").append(wrongDatesTemplate);
+    }
+
+  });
+};
