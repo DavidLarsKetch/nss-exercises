e94c9a8cb2cb4f2e2190afb96990939d967655ee
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-08 09:36:17 -0600

    Defines NASA Promise in xhr.js

--- assets/js/xhr.js
+++ assets/js/xhr.js
@@ -5,3 +5,18 @@ const random = require('lodash/random');
 const from = "Cohort-23";
 const slurs = ["back", "blackadder", "bus", "cocksplat", "donut", "gfy", "ing", "keep", "king", "off", "outside", "problem", "yoda", "you"];
 
+module.exports.nasa = selection => {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url:
+      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${selection.from}&end_date=${selection.to}&api_key=DEMO_KEY`
+    })
+    .done(data => {
+      resolve(data);
+    })
+    .fail(err => {
+      reject(err);
+    });
+  });
+};
+
