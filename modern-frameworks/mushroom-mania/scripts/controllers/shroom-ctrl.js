d13cf7cb301cf70b98039debba37b5070949ed07
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-26 11:54:28 -0600

    Refactors filter in mushrooms.html to check edible prop instead of redefining .shrooms in shroom-ctrl.js

--- modern-frameworks/mushroom-mania/scripts/controllers/shroom-ctrl.js
+++ modern-frameworks/mushroom-mania/scripts/controllers/shroom-ctrl.js
@@ -3,25 +3,12 @@
 angular.module("ShroomApp").controller("ShroomCtrl", function($scope, ShroomFactory) {
   $scope.title = "Mushroom Mania";
 
+  $scope.check;
+
   ShroomFactory.getShrooms()
   .then(shroomData => {
     let firebaseID = Object.keys(shroomData.data);
     let shrooms = Object.keys(shroomData.data[firebaseID]);
-    $scope.allShrooms = shrooms.map(shroom => shroomData.data[firebaseID][shroom]);
-    $scope.shrooms = $scope.allShrooms;
-    $scope.edible = $scope.allShrooms.filter(shroom => shroom.edible === true);
-    $scope.poisonous = $scope.allShrooms.filter(shroom => shroom.edible === false);
+    $scope.shrooms = shrooms.map(shroom => shroomData.data[firebaseID][shroom]);
   });
-
-  $scope.getAll = () => {
-    $scope.shrooms = $scope.allShrooms;
-  };
-
-  $scope.getEdible = () => {
-    $scope.shrooms = $scope.edible;
-  };
-
-  $scope.getPoisonous = () => {
-    $scope.shrooms = $scope.poisonous;
-  };
 });
