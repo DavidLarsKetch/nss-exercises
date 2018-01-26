e1723b73999fc836664dc4e96034a3336be81a06
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-26 12:47:59 -0600

    Defines bookFactory.js getting data from local JSON file

--- modern-frameworks/world-travel/app/factories/bookFactory.js
+++ modern-frameworks/world-travel/app/factories/bookFactory.js
@@ -1,2 +1,13 @@
 "use strict";
 
+angular.module("GuidesApp").factory("BookFactory", function($q, $http) {
+  const getBooks = () => {
+    return $q((resolve, reject) =>
+      $http.get('../../data/guides.json')
+      .then(response => resolve(response))
+      .catch(err => reject(err))
+    );
+  };
+
+  return {getBooks};
+});
