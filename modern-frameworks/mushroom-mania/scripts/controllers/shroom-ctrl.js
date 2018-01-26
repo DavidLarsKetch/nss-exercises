1f2e5eb4df4d301075cc5174e6cccb651d16a870
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-26 10:00:47 -0600

    Defines ShroomFactory.getShrooms().then() to extract shroom objs & sort into edible/poisonous vars; defines getAll(), getEdible(), getPoisonous()

--- modern-frameworks/mushroom-mania/scripts/controllers/shroom-ctrl.js
+++ modern-frameworks/mushroom-mania/scripts/controllers/shroom-ctrl.js
@@ -1,5 +1,28 @@
 "use strict";
 
-app.controller("ShroomCtrl", function($scope) {
+angular.module("ShroomApp").controller("ShroomCtrl", function($scope, ShroomFactory) {
   $scope.title = "Mushroom Mania";
+
+  ShroomFactory.getShrooms()
+  .then(shroomData => {
+    console.log(shroomData.data);
+    let firebaseID = Object.keys(shroomData.data);
+    let shrooms = Object.keys(shroomData.data[firebaseID]);
+    $scope.allShrooms = shrooms.map(shroom => shroomData.data[firebaseID][shroom]);
+    $scope.shrooms = $scope.allShrooms;
+    $scope.edible = $scope.allShrooms.filter(shroom => shroom.edible === true);
+    $scope.poisonous = $scope.allShrooms.filter(shroom => shroom.edible === false);
+  });
+
+  $scope.getAll = () => {
+    $scope.shrooms = $scope.allShrooms;
+  };
+
+  $scope.getEdible = () => {
+    $scope.shrooms = $scope.edible;
+  };
+
+  $scope.getPoisonous = () => {
+    $scope.shrooms = $scope.poisonous;
+  };
 });
