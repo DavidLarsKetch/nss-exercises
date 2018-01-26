4974368e3dbcd1ea549591b20f9426d91e136a31
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-26 12:47:10 -0600

    Defines angular.module(GuidesApp) in app.js, binding partial to controller

--- modern-frameworks/world-travel/app/app.js
+++ modern-frameworks/world-travel/app/app.js
@@ -1,2 +1,10 @@
 "use strict";
 
+angular.module("GuidesApp", ['ngRoute'])
+.config($routeProvider => {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/guides.html',
+    controller: 'BookCtrl'
+  });
+});
