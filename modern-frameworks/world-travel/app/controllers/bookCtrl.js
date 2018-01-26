5c053ae41469e05682395b344515f0e170d44fdc
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-26 12:47:41 -0600

    Defines bookCtrl.js processing data from factory

--- modern-frameworks/world-travel/app/controllers/bookCtrl.js
+++ modern-frameworks/world-travel/app/controllers/bookCtrl.js
@@ -1,2 +1,8 @@
 "use strict";
 
+angular.module("GuidesApp").controller("BookCtrl", function($scope, BookFactory) {
+  BookFactory.getBooks()
+  .then(bookData => {
+    $scope.guides = bookData.data.guides;
+  });
+});
