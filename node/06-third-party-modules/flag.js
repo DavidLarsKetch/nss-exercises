803b70c87bb5b99414712675716d3a1b51bb85fc
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-04 13:36:22 -0600

    Completes exercise: also adds over 25 other flags, no shame

--- node/06-third-party-modules/flag.js
+++ node/06-third-party-modules/flag.js
@@ -1,180 +1,26 @@
 #!/usr/bin/env node
 
 "use strict";
-const countries = ['belgium', 'colombia', 'france', 'germany', 'netherlands', 'peru', 'russia', 'usa'];
-const country = process.argv[2];
+const input = process.argv[2];
 
-if (!countries.includes(country)) {
-  process.stdout.write(`'Usage: ./index.js ${countries}'\n`);
+const {flags} = require('./countries');
+const {red} = require('chalk');
+
+let countries = [...Object.keys(flags)];
+
+if (input !== "rand" && !countries.includes(input)) {
+  let msg = red(`Options:`);
+  countries = countries.map((elm, idx) => `\n  ${idx + 1}.  ${elm}`);
+  process.stdout.write(`${msg}${countries}\n`);
+  process.exit();
+} else if (input === "rand") {
+  let rand = Math.floor((Math.random() * countries.length));
+  let input = countries[rand];
+  let flag = flags[input](); 
+  process.stdout.write(`${input}\n${flag}\n`);
+  process.exit();
+} else {
+  let flag = flags[input]();
+  process.stdout.write(`${flag}\n`);
   process.exit();
 }
-
-const {bgBlack, bgBlue, bgRed, bgWhite, bgYellow, bgYellowBright, white}  = require('chalk');
-
-const striper = spaces => {
-  let space = ``;
-  for (let i = 0; i < spaces; i++) {
-    space += ` `;
-  }
-  return space;
-};
-
-const belgiumBlackStripe = bgBlack(striper(16));
-const belgiumYellowStripe = bgYellowBright(striper(16));
-const franceBlueStripe = bgBlue(striper(16));
-const franceWhiteStripe = bgWhite(striper(16));
-const fullBlackStripe = bgBlack(striper(50));
-const fullBlueStripe = bgBlue(striper(50));
-const fullOrangeStripe = bgYellow(striper(50));
-const fullRedStripe = bgRed(striper(50));
-const fullWhiteStripe = bgWhite(striper(50));
-const fullYellowStripe = bgYellowBright(striper(50));
-const horizontalRedStripe = bgRed(striper(16));
-const starMargin = bgBlue(` `);
-const usaRedStripe = bgRed(striper(35));
-const usaWhiteStripe = bgWhite(striper(35));
-const whiteStar = bgBlue(white(`\u2606`));
-
-const sevenStars = `${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}`;
-const sixStars = `${starMargin}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${whiteStar}${starMargin}${starMargin}`;
-
-
-const flags = {
-  belgium: () => {
-    process.stdout.write(`${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}
-${belgiumBlackStripe}${belgiumYellowStripe}${horizontalRedStripe}\n`);
-  },
-  colombia: () => {
-    process.stdout.write(`${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}\n`);
-  },
-  france: () => {
-    process.stdout.write(`${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}
-${franceBlueStripe}${franceWhiteStripe}${horizontalRedStripe}\n`);
-  },
-  germany: () => {
-    process.stdout.write(`${fullBlackStripe}
-${fullBlackStripe}
-${fullBlackStripe}
-${fullBlackStripe}
-${fullBlackStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullYellowStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}\n`);
-  },
-  netherlands: () => {
-    process.stdout.write(`${fullOrangeStripe}
-${fullOrangeStripe}
-${fullOrangeStripe}
-${fullOrangeStripe}
-${fullOrangeStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}\n`);
-  },
-  peru: () => {
-    process.stdout.write(`${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}\n`);
-  },
-  russia: () => {
-    process.stdout.write(`${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullRedStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullWhiteStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}
-${fullBlueStripe}\n`);
-  },
-  usa: () => {
-    process.stdout.write(`${sevenStars}${usaRedStripe}
-${sixStars}${usaWhiteStripe}
-${sevenStars}${usaRedStripe}
-${sixStars}${usaWhiteStripe}
-${sevenStars}${usaRedStripe}
-${sixStars}${usaWhiteStripe}
-${sevenStars}${usaRedStripe}
-${fullWhiteStripe}
-${fullRedStripe}
-${fullWhiteStripe}
-${fullRedStripe}
-${fullWhiteStripe}
-${fullRedStripe}\n`);
-  }
-};
-
-flags[country]();
-process.exit();
