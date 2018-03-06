e01898f3c92063848f6de8fa5576dd687818eeae
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-06 08:43:21 -0600

    Completes exercise, task 6 - 11

--- node/11-sql-queries/queries.sql
+++ node/11-sql-queries/queries.sql
@@ -14,3 +14,14 @@ SELECT ar.ArtistName, al.Title "AlbumTitle", s.Title "SongTitle"
  ON s.ArtistId = al.ArtistId AND s.ArtistId = ar.ArtistId 
  WHERE s.Title="In the Street";
 
+SELECT COUNT(*) FROM Song GROUP BY AlbumId
+
+SELECT COUNT(*) FROM Song GROUP BY ArtistId
+
+SELECT COUNT(*) FROM Song GROUP BY GenreId
+
+SELECT Title, MAX(AlbumLength) AS AlbumLength FROM Album
+
+SELECT Title, MAX(SongLength) AS SongLength FROM Song
+
+SELECT a.Title AS AlbumTitle, s.Title AS SongTitle, MAX(s.SongLength) AS SongLength FROM Song s, Album a WHERE s.AlbumId = a.AlbumId
