6b9365d5ae1dc6f41fae025d9b1b771a9985d1dc
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-06 10:09:11 -0600

    Refactors exercise 11 to follow reqs more closely

--- node/11-sql-queries/queries.sql
+++ node/11-sql-queries/queries.sql
@@ -14,14 +14,26 @@ SELECT ar.ArtistName, al.Title "AlbumTitle", s.Title "SongTitle"
  ON s.ArtistId = al.ArtistId AND s.ArtistId = ar.ArtistId 
  WHERE s.Title="In the Street";
 
-SELECT COUNT(*) FROM Song GROUP BY AlbumId
+SELECT COUNT(*) "Song Count", Album.Title "Album Title"
+ FROM Song 
+ JOIN Album
+ ON Song.AlbumId = Album.AlbumId
+ GROUP BY Song.AlbumId
 
-SELECT COUNT(*) FROM Song GROUP BY ArtistId
+SELECT COUNT(*) "Song Count", Artist.ArtistName "Artist"
+ FROM Song 
+ JOIN Artist
+ ON Song.ArtistId = Artist.ArtistId
+ GROUP BY Song.ArtistId
 
-SELECT COUNT(*) FROM Song GROUP BY GenreId
+SELECT COUNT(*) "Song Count", Genre.Label "Genre"
+ FROM Song 
+ JOIN Genre
+ ON Song.GenreId = Genre.GenreId
+ GROUP BY Song.GenreId
 
-SELECT Title, MAX(AlbumLength) AS AlbumLength FROM Album
+SELECT Title, MAX(AlbumLength) "AlbumLength" FROM Album
 
-SELECT Title, MAX(SongLength) AS SongLength FROM Song
+SELECT Title, MAX(SongLength) "SongLength" FROM Song
 
 SELECT a.Title AS AlbumTitle, s.Title AS SongTitle, MAX(s.SongLength) AS SongLength FROM Song s, Album a WHERE s.AlbumId = a.AlbumId
