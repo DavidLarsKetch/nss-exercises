796b3cd6d7d5d62176ee9261080e7b18402ae861
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-13 09:19:26 -0500

    Completes delivered.js & delivered.test.js

--- node/17-tdd-bag-o-loot/test/delivered.test.js
+++ node/17-tdd-bag-o-loot/test/delivered.test.js
@@ -4,12 +4,32 @@ const { assert: { equal } } = require('chai');
 
 const { markAsDelivered } = require('../lib/delivered');
 
+const { createTables } = require('../lib/db');
+
 describe('delivered module', () => {
   describe('markAsDelivered()', () => {
+    beforeEach(function(done) {
+      createTables()
+      .then(() => done());
+    });
+
     it('should return a confirmation', () => {
       const expected = 'Gift delivered';
-      return markAsDelivered()
-      .then(msg => equal(expected, msg))
+      return markAsDelivered('fatim')
+      .then(({msg}) => equal(expected, msg))
+    });
+
+    it('should change the value of delivered from 0 to number of child\'s gifts',
+      () => {
+      return markAsDelivered('fatim') // Fatim has 2 gifts
+      .then(({num}) => equal(2, num))
+      }
+    );
+
+    it('should return the expected message on invalid child option', () => {
+      const expected = 'Not a child';
+      return markAsDelivered('franklin')
+      .then(({msg}) => equal(msg, expected))
     });
   });
 });
