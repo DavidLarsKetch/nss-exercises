9bc92599ba6397bdd5f312b11337017791f66a61
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-13 14:56:22 -0500

    Fixes list.js & list.test.js

--- node/17-tdd-bag-o-loot/lib/list.js
+++ node/17-tdd-bag-o-loot/lib/list.js
@@ -8,12 +8,13 @@ const db = new Database('gifts.sqlite', err => {
   );
 });
 
-module.exports.listChildsToy = child =>
+module.exports.listChildsToy = ({child}) =>
   new Promise((resolve, reject) => {
     db.all(`SELECT * FROM gifts WHERE child="${child}"`,
     function(err, data) {
       if (err) reject(err);
-      resolve(data);
+      let gifts = data.map(({gift}) => gift);
+      resolve({data, msg: `${child}'s toys: ${gifts}`});
     })
   });
 
@@ -21,8 +22,11 @@ module.exports.listGoodChildren = () =>
   new Promise((resolve, reject) => {
     db.all(`SELECT child FROM gifts`,
     (err, data) => {
-      let children = data.map(({child}) => child);
+      let children = data
+        .map(({child}) => child)
+        .sort()
+        .filter((elm, idx, arr) => !idx || elm != arr[idx - 1]);
       if (err) reject(err);
-      resolve(children.sort().filter((elm, idx, arr) => !idx || elm != arr[idx - 1]));
+      resolve({data: children, msg: `All the good children: ${children}`});
     })
   });
--- node/17-tdd-bag-o-loot/test/list.test.js
+++ node/17-tdd-bag-o-loot/test/list.test.js
@@ -12,25 +12,25 @@ describe('list module', () => {
     ];
 
     it('should return an array', () => {
-      return listChildsToy()
-      .then(gifts => isArray(gifts));
+      return listChildsToy({child: 'fatim'})
+      .then(({data}) => isArray(data));
     });
 
     it('should contain a collection of objects', () => {
-      return listChildsToy('fatim')
-      .then(gifts => isObject(gifts[0]));
+      return listChildsToy({child: 'fatim'})
+      .then(({data}) => isObject(data[0]));
     })
 
     it('should return a collection of objects for the specified child', () => {
-      return listChildsToy('fatim')
-      .then(gifts => deepEqual(gifts[0].gift, expected[0].gift))
+      return listChildsToy({child: 'fatim'})
+      .then(({data}) => deepEqual(data[0].gift, expected[0].gift))
     });
   });
 
   describe('listGoodChildren', () => {
     it('should return an array with no duplicates', () => {
       return listGoodChildren()
-      .then(data => data.sort().map((a,b) => notEqual(a, data[b + 1])));
+      .then(({data}) => data.sort().map((a,b) => notEqual(a, data[b + 1])));
     });
   });
 });
