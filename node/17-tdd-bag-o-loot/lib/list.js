0a5fe504b00b2ca96f57dbf2206b9f5679caab65
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-13 09:18:48 -0500

    Completes list.js & list.test.js

--- node/17-tdd-bag-o-loot/lib/list.js
+++ node/17-tdd-bag-o-loot/lib/list.js
@@ -1,11 +1,28 @@
 "use strict";
+const { errorEmitter } = require('./errors');
+
+const { Database } = require('sqlite3').verbose();
+const db = new Database('gifts.sqlite', err => {
+  if (err) errorEmitter.emit('db_err',
+    () => console.log(new Error("failed connection"))
+  );
+});
 
 module.exports.listChildsToy = child =>
   new Promise((resolve, reject) => {
-    resolve([]);
+    db.all(`SELECT * FROM gifts WHERE child="${child}"`,
+    function(err, data) {
+      if (err) reject(err);
+      resolve(data);
+    })
   });
 
 module.exports.listGoodChildren = () =>
   new Promise((resolve, reject) => {
-    resolve([]);
+    db.all(`SELECT child FROM gifts`,
+    (err, data) => {
+      let children = data.map(({child}) => child);
+      if (err) reject(err);
+      resolve(children.sort().filter((elm, idx, arr) => !idx || elm != arr[idx - 1]));
+    })
   });
--- node/17-tdd-bag-o-loot/test/list.test.js
+++ node/17-tdd-bag-o-loot/test/list.test.js
@@ -1,21 +1,36 @@
 "use strict";
 
-const { assert: { isArray } } = require('chai');
+const { assert: { deepEqual, isArray, isObject, notEqual} } = require('chai');
 
 const { listChildsToy, listGoodChildren } = require('../lib/list');
 
 describe('list module', () => {
   describe('listChildsToy()', () => {
+    let expected = [
+      {child: "fatim", gift: "doll", delivered: 0},
+      {child: "fatim", gift: "stick", delivered: 0}
+    ];
+
     it('should return an array', () => {
-      return listGoodChildren()
+      return listChildsToy()
       .then(gifts => isArray(gifts));
+    });
+
+    it('should contain a collection of objects', () => {
+      return listChildsToy('fatim')
+      .then(gifts => isObject(gifts[0]));
+    })
+
+    it('should return a collection of objects for the specified child', () => {
+      return listChildsToy('fatim')
+      .then(gifts => deepEqual(gifts[0].gift, expected[0].gift))
     });
   });
 
   describe('listGoodChildren', () => {
-    it('should return an array', () => {
+    it('should return an array with no duplicates', () => {
       return listGoodChildren()
-      .then(kids => isArray(kids));
+      .then(data => data.sort().map((a,b) => notEqual(a, data[b + 1])));
     });
   });
 });
