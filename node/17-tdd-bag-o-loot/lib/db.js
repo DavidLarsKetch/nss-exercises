7dbcfbe4a531b3f766da39473757db1e38d57d48
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-13 14:57:06 -0500

    Completes exercise 17

--- node/17-tdd-bag-o-loot/lib/db.js
+++ node/17-tdd-bag-o-loot/lib/db.js
@@ -3,68 +3,50 @@
 const { errorEmitter } = require('./errors');
 
 const { gifts } = require('../data/children');
+
 const { Database } = require('sqlite3').verbose();
 let db;
 
-
-
-(function createDb() {
-  db = new Database('gifts.sqlite', () => {
+(function createDB() {
+  db = new Database('gifts.sqlite', () =>
     module.exports.createTables()
-    .then( (data) => {
-      console.log("child ids", data);
-    })
-    .catch( (err) => {
-      console.log("oops", err);
-    });
-  });
-}());
-
-module.exports.createTables = () => {
-  return new Promise((resolve, reject) => {
-    dropTable()
-    .then(() =>
-      db.run(
-        `CREATE TABLE IF NOT EXISTS gifts (id INTEGER PRIMARY KEY, child TEXT, gift TEXT, delivered INT)`,
-          err => {
-            if (err) return reject(err);
-            resolve(insertRows());
-          }
+    .then(data => {})
+    .catch(err =>
+      errorEmitter.emit('db_err',
+        () => console.log(new Error('failed to create tables'))
       )
     )
+  );
+})();
 
-  });
-};
-
-const dropTable = () =>
+module.exports.createTables = () =>
   new Promise((resolve, reject) =>
-    db.run(`DROP TABLE IF EXISTS gifts`,
-      err => {
-        if (err) return reject(err);
-        resolve();
-      }
-    )
+    db.serialize(function() {
+      db.run(`DROP TABLE IF EXISTS gifts`);
+      db.run(`CREATE TABLE IF NOT EXISTS gifts (id INTEGER PRIMARY KEY, child TEXT, gift TEXT, delivered INT)`,
+        err => {
+          if (err) return reject(err);
+          resolve(insertRows());
+        });
+    })
   );
 
-const insertRows = () => {
-  return Promise.all(
-    gifts.map(
-      ({ child, gift }) => {
-          return new Promise((resolve, reject) => {
-          db.run(
-            `INSERT INTO gifts VALUES (
-              null,
-              "${child}",
-              "${gift}",
-              0
-            )`,
-            function(err) {
-              if (err) return reject(err);
-              resolve(this.lastID);
-            }
-          );
-        });
-      }
+const insertRows = () =>
+  Promise.all(
+    gifts.map(({ child, gift }) =>
+      new Promise((resolve, reject) =>
+        db.run(
+          `INSERT INTO gifts VALUES (
+            null,
+            "${child}",
+            "${gift}",
+            0
+          )`,
+          function(err) {
+            if (err) return reject(err);
+            resolve(this.lastID);
+          }
+        )
+      )
     )
   );
-};
