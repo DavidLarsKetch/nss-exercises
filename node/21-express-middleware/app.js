fa4b8a09f6a72d6ab22b696d8492d92e3eaf50ce
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-14 15:43:16 -0500

    Completes Exercise 21

--- node/21-express-middleware/app.js
+++ node/21-express-middleware/app.js
@@ -5,10 +5,29 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const egg = require('./ascii');
 
-app.use('/home', );
-app.use('/see-our-chickens', );
-app.use('/see-our-eggs', );
+const currentTime = (req, res, next) => {
+  let d = new Date;
+  req.currentTime = d.toString();
+  next();
+};
+
+app.use(express.static(__dirname + '/public', {extensions: 'html'}));
+
+app.use(currentTime);
+
+app.use('/:maybe_egg', (req, res, next) => {
+  if (req.params.maybe_egg.includes('egg')) {
+    console.log(`You found the Easter Egg at ${req.currentTime}`);
+    console.log(egg);
+  }
+  let err = new Error('nothing here!');
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => res.send('Nothing found here!'));
 
 const port = process.env.PORT || 8080;
 
