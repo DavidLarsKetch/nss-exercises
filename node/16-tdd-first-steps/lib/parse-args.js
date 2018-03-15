49aa590d598350e29279edc87172654a7aed515d
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-14 21:44:47 -0500

    Completes Exercise 16

---
+++ node/16-tdd-first-steps/lib/parse-args.js
@@ -0,0 +1,4 @@
+"use strict";
+
+module.exports = args => args.map(arg => +arg).filter(num => num);
+
---
+++ node/16-tdd-first-steps/lib/subtract.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports.subtract = nums => nums.reduce((a,b) => a - b);
