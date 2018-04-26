22943e7beae980747ed101191397da26603ee9ce
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-26 08:01:01 -0500

    Completes Py Ex. 1

--- python/01-dictionaries/stocks.py
+++ python/01-dictionaries/stocks.py
@@ -2,22 +2,27 @@ stockDict = { 'GOOG': 'Google', 'FB': 'Facebook', 'SPT': 'Spotify', 'NTFX': 'Net
 purchases = [ ( 'GOOG', 134, '10-10-2010', 242 ),
         ( 'FB', 21, '20-12-2011', 23 ),
         ( 'SPT', 1, '01-04-2018', 232 ),
-        ( 'NTFX', 1000, '01-01-2015', 22 )
+        ( 'NTFX', 1000, '01-01-2015', 22 ),
+        ( 'SPT', 3, '01-30-2017', 244 )
 ]
 
 def purchases_by_each():
-    '''Computes total purchase price for each purchase & provides stock name
+    '''
+    Computes total purchase price for each purchase & provides stock name
 
     Returns: list
     '''
-    totals_each = []
-    for p in purchases:
-        totals_each.append( { stockDict[ p[0] ]: p[1] * p[3] } )
-
+    totals_each = [{ stockDict[ p[0] ]: p[1] * p[3] for p in purchases }]
     return(totals_each)
 
 def purchases_total_by_stock():
-    '''Computes total purchase price for all purchases of each stock
     '''
+    Computes total purchase price for all purchases of each stock
+    '''
+    totals_by_stock = { k : 0 for k in stockDict } 
+    for p in purchases:
+        totals_by_stock[ p[0] ] += (p[1] * p[3])
+    return(totals_by_stock)
 
 print(purchases_by_each())
+print(purchases_total_by_stock())
