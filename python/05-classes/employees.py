699b5da3462d0bfb0df9bb1af97a4b28a6ac9d2d
David Lars Ketch <davidlarsketch@gmail.com>
2018-04-27 12:06:07 -0500

    Completes Python Exercise 05

---
+++ python/05-classes/employees.py
@@ -0,0 +1,35 @@
+class Employee(object):
+    def __init__(self, name, title, start_date):
+        self.name = name
+        self.title = title
+        self.start_date = start_date
+
+    def get_name(self):
+        return self.name
+    def get_title(self):
+        return self.title
+    def get_start_date(self):
+        return self.start_date
+
+class Company(object):
+    def __init__(self, name, address): 
+        self.name = name
+        self.address = address
+        self.employees = set()
+
+    def get_name(self):
+        return self.name
+    def get_address(self):
+        return self.address
+    #def get_employees(self):
+    #    return self.employees
+
+if __name__ == '__main__':
+    Bangazon = Company('Bangazon', '123 Main St')
+    mary = Employee('Mary Ketch', 'CEO', '01-01-2018')
+    david = Employee('David Ketch', 'Janitor', '01-01-2018')
+    stephen = Employee('Stephen Ketch', 'Sales Associate', '01-01-2018')
+    Bangazon.employees.add(mary)
+    Bangazon.employees.add(david)
+    Bangazon.employees.add(stephen)
+    print(employee for employee in Bangazon.employees)
